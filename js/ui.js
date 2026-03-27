d813cee244e7aa16484fa44fd99e8cd3a382f0e7
Laucardona <lauris.042006@gmail.com>
2026-03-27 16:49:03 -0500

    feat: creacion ui

---
+++ js/ui.js
@@ -0,0 +1,33 @@
+import { getState } from "./state.js";
+
+export function renderShows(container) {
+    const { shows } = getState();
+
+    container.innerHTML = "";
+
+    shows.forEach(show => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <h3>${show.name}</h3>
+            <img src="${show.image?.medium || ''}" />
+            <button data-id="${show.id}">Favorito</button>
+        `;
+
+        container.appendChild(div);
+    });
+}
+
+export function renderFavorites(container, favorites) {
+    container.innerHTML = "";
+
+    favorites.forEach(show => {
+        const div = document.createElement("div");
+        div.textContent = show.name;
+        container.appendChild(div);
+    });
+}
+
+export function showMessage(msg) {
+    alert(msg);
+}
