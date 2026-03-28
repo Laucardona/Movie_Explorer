4753da5f63958133d0322946cf700d0b6a27c4d8
Laucardona <lauris.042006@gmail.com>
2026-03-27 23:28:01 -0500

    fix: agregando al main nueva funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,89 +1,33 @@
-import { getShows, searchShows } from "./service.js";
-import {
-    loadInitialState,
-    addFavoritePersist,
-    removeFavoritePersist,
-    saveSearchPersist
-} from "./persistence.js";
-
-import {
-    setState,
-    setShows,
-    getState
-} from "./state.js";
-
-import {
-    renderShows,
-    renderFavorites,
-    showMessage
-} from "./ui.js";
-
-// 📌 Elementos del DOM
-const container = document.getElementById("app");
-const favoritesContainer = document.getElementById("favorites");
-const searchInput = document.getElementById("search");
-const searchBtn = document.getElementById("btnSearch");
+const grid = document.getElementById("moviesGrid");
+const toggleBtn = document.getElementById("themeToggle");
 
 
-// 🚀 INIT
-async function init() {
-    const initial = loadInitialState();
-    setState(initial);
-
-    const shows = await getShows();
-    setShows(shows.slice(0, initial.itemsPerPage));
-
-    renderShows(container);
-    renderFavorites(favoritesContainer, initial.favorites);
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-mode");
+  toggleBtn.textContent = "☀︎";
 }
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 
-// 🔍 BUSCAR
-searchBtn.addEventListener("click", async () => {
-    const query = searchInput.value;
+  const isLight = document.body.classList.contains("light-mode");
 
-    const result = saveSearchPersist(query);
-    showMessage(result.message);
-
-    const shows = await searchShows(query);
-    setShows(shows);
-
-    renderShows(container);
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+  toggleBtn.textContent = isLight ? "☀︎" : "⏾";
 });
 
 
-// ❤️ FAVORITOS (delegación)
-container.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON") {
-        const id = e.target.dataset.id;
+function renderMovies() {
+  grid.innerHTML = "";
 
-        const state = getState();
-        const show = state.shows.find(s => s.id == id);
+  movies.forEach(m => {
+    grid.innerHTML += `
+      <div class="card">
+        <img src="${m.image}">
+        <h3>${m.title}</h3>
+      </div>
+    `;
+  });
+}
 
-        if (!show) return;
-
-        const result = addFavoritePersist(show);
-        showMessage(result.message);
-
-        // actualizar favoritos en pantalla
-        const newFavorites = loadInitialState().favorites;
-        renderFavorites(favoritesContainer, newFavorites);
-    }
-});
-
-
-// ❌ ELIMINAR FAVORITO
-favoritesContainer.addEventListener("click", (e) => {
-    if (e.target.dataset.id) {
-        const id = e.target.dataset.id;
-
-        const result = removeFavoritePersist(id);
-        showMessage(result.message);
-
-        const newFavorites = loadInitialState().favorites;
-        renderFavorites(favoritesContainer, newFavorites);
-    }
-});
-
-
-init();
+renderMovies();
